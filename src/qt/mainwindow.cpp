c0bbd4fab78b612525b3633d60eec32655d013de
ichea88 <mine@myemail.com>
2015-04-25 13:23:13 +0200

    init QT branch

---
+++ src/qt/mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.hpp"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_PrevButtonInput_clicked() {
+    ui->NextButtonInput->setText("CIAO");
+}
