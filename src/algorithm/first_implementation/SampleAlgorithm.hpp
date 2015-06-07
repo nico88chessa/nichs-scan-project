57461533f27fbed7da10df81842a5e2adf05f5c7
ichea88 <mine@myemail.com>
2015-06-07 11:16:18 +0200

    divide header/implementation sample algorithm

--- src/algorithm/first_implementation/SampleAlgorithm.hpp
+++ src/algorithm/first_implementation/SampleAlgorithm.hpp
@@ -19,14 +19,8 @@ public:
     virtual ~SampleAlgorithm() { };
 
 protected:
-    std::vector<cv::Mat> process(const cv::Mat& input) {
-        std::vector<cv::Mat> vet;
-        for (int i=0; i<NUM_IMAGES; i++) {
-            vet.push_back(input.clone());
-        }
-        usleep(2*1000);
-        return vet;
-    }
+    std::vector<cv::Mat> process(const cv::Mat& input);
+
 };
 
 #endif /* ALGORITHM_FIRST_IMPLEMENTATION_SAMPLEALGORITHM_HPP_ */
